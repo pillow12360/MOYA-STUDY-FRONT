205ac71a8f6d313fb6fc52c9672bdb616449d8a4
pillow12360 <ehdcks1256@naver.com>
2024-08-26 22:02:51 +0900

    파이어 베이스 연동

--- src/layouts/App.tsx
+++ src/layouts/App.tsx
@@ -3,11 +3,13 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-d
 import { ModalProvider } from '@contexts/ModalContext';
 import SignupForm from '@pages/auth/SignupForm';
 import LoginForm from '@pages/auth/LoginForm';
+import Home from '@pages/home/Home';
 
 const App = () => {
   return (
     <ModalProvider>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/signup" element={<SignupForm />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/" element={<Navigate to="/signup" replace />} />
