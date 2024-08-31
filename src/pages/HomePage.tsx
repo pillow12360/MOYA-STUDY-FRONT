f3b26129ca09c6299b1a8272c1aaf0030dd72fc8
pillow12360 <ehdcks1256@naver.com>
2024-08-31 12:44:08 +0900

    HomePage 컴포넌트 팀 구성 카드 수정

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import { Typography, Box, Container, Grid, Card, CardContent } from '@mui/material';
+import React, { useState } from 'react';
+import { Typography, Box, Container, Grid, Card, CardContent, Collapse, IconButton } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 interface HomePageProps {
   mode: 'light' | 'dark';
@@ -8,7 +9,15 @@ interface HomePageProps {
 }
 
 const HomePage = ({ mode, toggleColorMode }: HomePageProps) => {
+  const [expanded, setExpanded] = useState({ frontend: false, backend: false, cloud: false });
   const theme = useTheme();
+
+  const handleExpandClick = (section: keyof typeof expanded) => {
+    setExpanded((prevState) => ({
+      ...prevState,
+      [section]: !prevState[section],
+    }));
+  };
 
   return (
     <Box sx={{ minHeight: '100vh', textAlign: 'center' }}>
@@ -41,8 +50,8 @@ const HomePage = ({ mode, toggleColorMode }: HomePageProps) => {
           </Typography>
           <Grid container spacing={4}>
             <Grid item xs={12} sm={4}>
-              <Card>
-                <CardContent>
+              <Card sx={{ maxWidth: 345, marginBottom: '10px' }}>
+                <CardContent onClick={() => handleExpandClick('frontend')} sx={{ cursor: 'pointer' }}>
                   <img
                     src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg"
                     alt="React"
@@ -54,27 +63,99 @@ const HomePage = ({ mode, toggleColorMode }: HomePageProps) => {
                   <Typography variant="body1">
                     React, TypeScript, MUI를 사용하여 사용자 친화적인 인터페이스를 만듭니다.
                   </Typography>
+                  <IconButton
+                    onClick={() => handleExpandClick('frontend')}
+                    aria-expanded={expanded.frontend}
+                    aria-label="show more"
+                    sx={{
+                      transform: expanded.frontend ? 'rotate(180deg)' : 'rotate(0deg)',
+                      transition: 'transform 0.3s',
+                    }}
+                  >
+                    <ExpandMoreIcon />
+                  </IconButton>
                 </CardContent>
+                <Collapse in={expanded.frontend} timeout="auto" unmountOnExit>
+                  <CardContent
+                    sx={{
+                      display: 'flex',
+                      flexDirection: 'column',
+                      alignItems: 'center',
+                      padding: '20px',
+                      backgroundColor: '#f0f0f0',
+                      borderRadius: '10px',
+                      boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+                      textAlign: 'center',
+                    }}
+                  >
+                    <Typography variant="h6" component="div" sx={{ fontWeight: 'bold' }}>
+                      한동찬
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" sx={{ marginBottom: '10px' }}>
+                      프론트엔드 개발자
+                    </Typography>
+                    <Typography variant="body1" sx={{ fontStyle: 'italic' }}>
+                      경력: 신입
+                    </Typography>
+                  </CardContent>
+                </Collapse>
               </Card>
             </Grid>
+
             <Grid item xs={12} sm={4}>
-              <Card>
-                <CardContent>
+              <Card sx={{ maxWidth: 345, marginBottom: '10px' }}>
+                <CardContent onClick={() => handleExpandClick('backend')} sx={{ cursor: 'pointer' }}>
                   <img
-                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDBnrqr2tLtfIEgfzjYUqOFHdQTM-TIKXWug&s"
+                    src="https://static-00.iconduck.com/assets.00/spring-icon-1024x1023-ljxx8bf7.png"
                     alt="Spring"
                     style={{ width: '50px' }}
                   />
                   <Typography variant="h5" component="h3" sx={{ marginTop: '15px' }}>
                     Back-End
                   </Typography>
-                  <Typography variant="body1">Spring, MySql와의 통합을 통해 강력한 서버 로직을 구현합니다.</Typography>
+                  <Typography variant="body1">Spring 및 MySQL의 통합으로 안정적인 백엔드 api 배포</Typography>
+                  <IconButton
+                    onClick={() => handleExpandClick('backend')}
+                    aria-expanded={expanded.backend}
+                    aria-label="show more"
+                    sx={{
+                      transform: expanded.backend ? 'rotate(180deg)' : 'rotate(0deg)',
+                      transition: 'transform 0.3s',
+                    }}
+                  >
+                    <ExpandMoreIcon />
+                  </IconButton>
                 </CardContent>
+                <Collapse in={expanded.backend} timeout="auto" unmountOnExit>
+                  <CardContent
+                    sx={{
+                      display: 'flex',
+                      flexDirection: 'column',
+                      alignItems: 'center',
+                      padding: '20px',
+                      backgroundColor: '#f0f0f0',
+                      borderRadius: '10px',
+                      boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+                      textAlign: 'center',
+                    }}
+                  >
+                    <Typography variant="h6" component="div" sx={{ fontWeight: 'bold' }}>
+                      한동찬
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" sx={{ marginBottom: '10px' }}>
+                      백엔드 개발자
+                    </Typography>
+                    <Typography variant="body1" sx={{ fontStyle: 'italic' }}>
+                      경력: 신입
+                    </Typography>
+                  </CardContent>
+                </Collapse>
               </Card>
             </Grid>
+
             <Grid item xs={12} sm={4}>
-              <Card>
-                <CardContent>
+              <Card sx={{ maxWidth: 345, marginBottom: '10px' }}>
+                <CardContent onClick={() => handleExpandClick('cloud')} sx={{ cursor: 'pointer' }}>
                   <img
                     src="https://cdn.icon-icons.com/icons2/2407/PNG/512/aws_icon_146074.png"
                     alt="AWS"
@@ -86,7 +167,39 @@ const HomePage = ({ mode, toggleColorMode }: HomePageProps) => {
                   <Typography variant="body1">
                     AWS 인프라를 활용해 확장성과 신뢰성을 갖춘 클라우드 환경을 구축합니다.
                   </Typography>
+                  <IconButton
+                    onClick={() => handleExpandClick('cloud')}
+                    aria-expanded={expanded.cloud}
+                    aria-label="show more"
+                    sx={{ transform: expanded.cloud ? 'rotate(180deg)' : 'rotate(0deg)', transition: 'transform 0.3s' }}
+                  >
+                    <ExpandMoreIcon />
+                  </IconButton>
                 </CardContent>
+                <Collapse in={expanded.cloud} timeout="auto" unmountOnExit>
+                  <CardContent
+                    sx={{
+                      display: 'flex',
+                      flexDirection: 'column',
+                      alignItems: 'center',
+                      padding: '20px',
+                      backgroundColor: '#f0f0f0',
+                      borderRadius: '10px',
+                      boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+                      textAlign: 'center',
+                    }}
+                  >
+                    <Typography variant="h6" component="div" sx={{ fontWeight: 'bold' }}>
+                      한동찬
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" sx={{ marginBottom: '10px' }}>
+                      클라우드 엔지니어
+                    </Typography>
+                    <Typography variant="body1" sx={{ fontStyle: 'italic' }}>
+                      경력: 신입
+                    </Typography>
+                  </CardContent>
+                </Collapse>
               </Card>
             </Grid>
           </Grid>
