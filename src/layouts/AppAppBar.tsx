5db7dcc6bba40590fad470b0713bcedf4f2123cd
pillow12360 <ehdcks1256@naver.com>
2024-09-23 13:03:43 +0900

    chore: 사용하지 않는 페이지 AppAppBar 바로가기 삭제

--- src/layouts/AppAppBar.tsx
+++ src/layouts/AppAppBar.tsx
@@ -99,15 +99,6 @@ function AppAppBar({ mode, toggleColorMode }: AppAppBarProps) {
               <MenuItem component={Link} to="/">
                 Home
               </MenuItem>
-              <MenuItem component={Link} to="/dashboard">
-                DashBoard
-              </MenuItem>
-              <MenuItem component={Link} to="/randomvoicechat">
-                Random Voice Chat
-              </MenuItem>
-              <MenuItem component={Link} to="/calendar">
-                Calendar
-              </MenuItem>
               <MenuItem component={Link} to="/tldraw">
                 Tldraw
               </MenuItem>
@@ -174,23 +165,8 @@ function AppAppBar({ mode, toggleColorMode }: AppAppBarProps) {
                 <MenuItem component={Link} to="/project">
                   Project
                 </MenuItem>
-                <MenuItem component={Link} to="/swagger">
-                  Swagger
-                </MenuItem>
-                <MenuItem component={Link} to="/dashboard">
-                  DashBoard
-                </MenuItem>
-                <MenuItem component={Link} to="/jira">
-                  Jira
-                </MenuItem>
-                <MenuItem component={Link} to="/slack">
-                  SLACK
-                </MenuItem>
-                <MenuItem component={Link} to="/about">
-                  About
-                </MenuItem>
-                <MenuItem component={Link} to="/contact">
-                  Contact
+                <MenuItem component={Link} to="/tldraw">
+                  Tldraw
                 </MenuItem>
                 <Divider />
                 {user ? (
