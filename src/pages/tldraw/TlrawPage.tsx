8be2f68cf9742de747c4da04b29c9b90e4c5cab3
pillow12360 <ehdcks1256@naver.com>
2024-09-23 12:11:59 +0900

    KAN-16 chore: 뷰포트 크기 수정

--- src/pages/tldraw/TlrawPage.tsx
+++ src/pages/tldraw/TlrawPage.tsx
@@ -3,7 +3,7 @@ import TldrawComponent from './TldrawComponent';
 
 const TldrawPage: React.FC = () => {
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
+    <div style={{ width: '80vw', height: '60vh' }}>
       <h1>tldraw 라이브러리 예제</h1>
       <TldrawComponent />
     </div>
