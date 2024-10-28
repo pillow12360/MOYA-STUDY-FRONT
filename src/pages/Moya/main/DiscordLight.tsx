e0bf5d76a413bfc6ff836dc9b4d463eba8a1281c
pillow12360 <ehdcks1256@naver.com>
2024-10-28 10:44:14 +0900

    KAN-30 fix: 렌더링 크기 뷰포트 넓이로 수정

--- src/pages/Moya/main/DiscordLight.tsx
+++ src/pages/Moya/main/DiscordLight.tsx
@@ -30,233 +30,175 @@ import {
 
 export const DiscordLight = (): JSX.Element => {
     return (
-        <Box display="flex" justifyContent="center" width="100%" bgcolor="#e3e5e8">
-            <Box width="1920px" height="900px" position="relative" bgcolor="#e3e5e8">
-                <Box
-                    position="absolute"
-                    width="1576px"
-                    height="900px"
-                    top={0}
-                    left="344px"
-                    bgcolor="white"
-                >
-                    <Box
-                        position="absolute"
-                        width="1544px"
-                        height="44px"
-                        top="836px"
-                        left="4px"
-                        borderRadius="8px"
-                        overflow="hidden"
-                        display="flex"
-                        alignItems="center"
-                        bgcolor="#ebedef"
-                    >
-                        <IconButton>
-                            <Avatar src="/div-icon.svg" />
-                        </IconButton>
-                        <Box flexGrow={1} display="flex" alignItems="center" pl={1}>
-                            <Typography variant="body1" color="#747f8d" fontFamily="Helvetica">
-                                Message #general
-                            </Typography>
-                        </Box>
-                        <IconButton>
-                            <Gif />
-                        </IconButton>
-                        <IconButton>
-                            <Image />
-                        </IconButton>
-                        <IconButton>
-                            <InsertEmoticon />
-                        </IconButton>
-                    </Box>
-                    <Box
-                        position="absolute"
-                        width="1540px"
-                        height="168px"
-                        top="652px"
-                        left="4px"
-                        component="img"
-                        src="/empty-message.png"
-                        alt="Empty message"
-                    />
-                    <Divider
-                        sx={{
-                            position: "absolute",
-                            width: "1576px",
-                            top: "48px",
-                            left: 0,
-                        }}
-                    />
-                    <Box
-                        position="absolute"
-                        width="1560px"
-                        height="48px"
-                        top={0}
-                        left="8px"
-                        display="flex"
-                        justifyContent="space-between"
-                        alignItems="center"
-                    >
-                        <Box display="flex" alignItems="center">
-                            <IconButton>
-                                <Tag />
-                            </IconButton>
-                            <Typography variant="h6" color="#060607" fontFamily="Helvetica" fontWeight="bold">
-                                general
-                            </Typography>
-                        </Box>
-                        <Box display="flex" alignItems="center">
-                            <IconButton>
-                                <Search />
-                            </IconButton>
-                            <TextField
+      <Box display="flex" justifyContent="center" width="100%" bgcolor="#e3e5e8" minHeight="100vh" overflow="hidden">
+          <Box display="flex" width="100%" maxWidth="1920px" height="900px" bgcolor="#e3e5e8">
+              {/* Left Sidebar */}
+              <Box width="72px" height="100%" bgcolor="#e3e5e8">
+                  <Box display="flex" flexDirection="column" alignItems="center">
+                      <Avatar src="/svg-homeicon.svg" sx={{ width: 48, height: 48, bgcolor: "white", borderRadius: 3, my: 2 }} />
+                      <Divider sx={{ width: "72px" }} />
+                      <Avatar src="/svg-circleicon.svg" sx={{ width: 48, height: 48, bgcolor: "white", borderRadius: 3, my: 2 }} />
+                      <Avatar src="/svg.svg" sx={{ width: 48, height: 48, bgcolor: "white", borderRadius: 3, my: 2 }} />
+                  </Box>
+              </Box>
+
+              {/* Channel List */}
+              <Box width="272px" height="100%" bgcolor="#f2f3f5">
+                  <Box display="flex" flexDirection="column" height="100%">
+                      <Box display="flex" justifyContent="space-between" alignItems="center" p={2} bgcolor="#f2f3f5">
+                          <Typography variant="h6" color="#060607" fontFamily="Helvetica" fontWeight="bold">
+                              Guild
+                          </Typography>
+                          <IconButton>
+                              <ExpandLess />
+                          </IconButton>
+                      </Box>
+                      <Divider />
+                      <List>
+                          <ListItem>
+                              <ListItemIcon>
+                                  <ExpandMore />
+                              </ListItemIcon>
+                              <ListItemText
+                                primary="TEXT CHANNELS"
+                                primaryTypographyProps={{
+                                    fontFamily: "Helvetica",
+                                    fontWeight: "bold",
+                                    color: "#6a7480",
+                                    variant: "body2",
+                                }}
+                              />
+                              <IconButton>
+                                  <Add />
+                              </IconButton>
+                          </ListItem>
+                          <ListItem component="div">
+                              <ListItemIcon>
+                                  <Tag />
+                              </ListItemIcon>
+                              <ListItemText
+                                primary="general"
+                                primaryTypographyProps={{
+                                    fontFamily: "Helvetica",
+                                    fontWeight: "medium",
+                                    color: "#060607",
+                                    variant: "body1",
+                                }}
+                              />
+                              <IconButton>
+                                  <Settings />
+                              </IconButton>
+                          </ListItem>
+                          <ListItem>
+                              <ListItemIcon>
+                                  <VolumeUp />
+                              </ListItemIcon>
+                              <ListItemText
+                                primary="General"
+                                primaryTypographyProps={{
+                                    fontFamily: "Helvetica",
+                                    fontWeight: "medium",
+                                    color: "#060607",
+                                    variant: "body1",
+                                }}
+                              />
+                              <IconButton>
+                                  <Settings />
+                              </IconButton>
+                          </ListItem>
+                      </List>
+                      <Divider />
+                      <Box display="flex" justifyContent="space-between" alignItems="center" px={2} py={1} bgcolor="#ebedef">
+                          <Box display="flex" alignItems="center">
+                              <Avatar src="/avatar.svg" />
+                              <Box ml={2}>
+                                  <Typography variant="body1" fontFamily="Helvetica" fontWeight="medium">
+                                      spencercamp
+                                  </Typography>
+                                  <Typography variant="body2" fontFamily="Helvetica" fontWeight="light">
+                                      #6709
+                                  </Typography>
+                              </Box>
+                          </Box>
+                          <Box>
+                              <IconButton>
+                                  <Settings />
+                              </IconButton>
+                              <IconButton>
+                                  <Mic />
+                              </IconButton>
+                              <IconButton>
+                                  <Headset />
+                              </IconButton>
+                          </Box>
+                      </Box>
+                  </Box>
+              </Box>
+
+              {/* Main Chat Area */}
+              <Box flex="1" height="100%" bgcolor="white">
+                  <Box display="flex" flexDirection="column" height="100%">
+                      <Box display="flex" justifyContent="space-between" alignItems="center" px={2} py={1} bgcolor="white">
+                          <Box display="flex" alignItems="center">
+                              <IconButton>
+                                  <Tag />
+                              </IconButton>
+                              <Typography variant="h6" color="#060607" fontFamily="Helvetica" fontWeight="bold">
+                                  general
+                              </Typography>
+                          </Box>
+                          <Box display="flex" alignItems="center">
+                              <TextField
                                 variant="outlined"
                                 size="small"
                                 placeholder="Search"
                                 InputProps={{
                                     startAdornment: (
-                                        <InputAdornment position="start">
-                                            <Search />
-                                        </InputAdornment>
+                                      <InputAdornment position="start">
+                                          <Search />
+                                      </InputAdornment>
                                     ),
                                 }}
-                            />
-                            <IconButton>
-                                <Settings />
-                            </IconButton>
-                            <IconButton>
-                                <Mic />
-                            </IconButton>
-                            <IconButton>
-                                <Headset />
-                            </IconButton>
-                            <IconButton>
-                                <ExitToApp />
-                            </IconButton>
-                        </Box>
-                    </Box>
-                </Box>
-                <Box position="absolute" width="272px" height="900px" top={0} left="72px" bgcolor="#f2f3f5">
-                    <Box position="absolute" width="272px" height="851px" top="49px" left={0} bgcolor="#f2f3f5">
-                        <Box
-                            position="absolute"
-                            width="272px"
-                            height="52px"
-                            top="799px"
-                            left={0}
-                            bgcolor="#ebedef"
-                            display="flex"
-                            alignItems="center"
-                            justifyContent="space-between"
-                            px={2}
-                        >
-                            <Box display="flex" alignItems="center">
-                                <Avatar src="/avatar.svg" />
-                                <Box ml={2}>
-                                    <Typography variant="body1" fontFamily="Helvetica" fontWeight="medium">
-                                        spencercamp
-                                    </Typography>
-                                    <Typography variant="body2" fontFamily="Helvetica" fontWeight="light">
-                                        #6709
-                                    </Typography>
-                                </Box>
-                            </Box>
-                            <Box display="flex" alignItems="center">
-                                <IconButton>
-                                    <Settings />
-                                </IconButton>
-                                <IconButton>
-                                    <Mic />
-                                </IconButton>
-                                <IconButton>
-                                    <Headset />
-                                </IconButton>
-                            </Box>
-                        </Box>
-                        <List>
-                            <ListItem>
-                                <ListItemIcon>
-                                    <ExpandMore />
-                                </ListItemIcon>
-                                <ListItemText
-                                    primary="TEXT CHANNELS"
-                                    primaryTypographyProps={{
-                                        fontFamily: "Helvetica",
-                                        fontWeight: "bold",
-                                        color: "#6a7480",
-                                        variant: "body2",
-                                    }}
-                                />
-                                <IconButton>
-                                    <Add />
-                                </IconButton>
-                            </ListItem>
-                            <ListItem component="button">
-                                <ListItemIcon>
-                                    <Tag />
-                                </ListItemIcon>
-                                <ListItemText
-                                    primary="general"
-                                    primaryTypographyProps={{
-                                        fontFamily: "Helvetica",
-                                        fontWeight: "medium",
-                                        color: "#060607",
-                                        variant: "body1",
-                                    }}
-                                />
-                                <IconButton>
-                                    <Settings />
-                                </IconButton>
-                            </ListItem>
-                            <ListItem component="button">
-                                <ListItemIcon>
-                                    <VolumeUp />
-                                </ListItemIcon>
-                                <ListItemText
-                                    primary="General"
-                                    primaryTypographyProps={{
-                                        fontFamily: "Helvetica",
-                                        fontWeight: "medium",
-                                        color: "#060607",
-                                        variant: "body1",
-                                    }}
-                                />
-                                <IconButton>
-                                    <Settings />
-                                </IconButton>
-                            </ListItem>
-                        </List>
-                    </Box>
-                    <Divider sx={{ position: "absolute", width: "272px", top: "48px", left: 0 }} />
-                    <Box position="absolute" width="240px" height="48px" top={0} left="16px" display="flex" alignItems="center" justifyContent="space-between">
-                        <Typography variant="h6" color="#060607" fontFamily="Helvetica" fontWeight="bold">
-                            Guild
-                        </Typography>
-                        <IconButton>
-                            <ExpandLess />
-                        </IconButton>
-                    </Box>
-                </Box>
-                <Box position="absolute" width="72px" height="900px" top={0} left={0} bgcolor="#e3e5e8">
-                    <Box position="absolute" width="72px" height="48px" top="154px" left={0} display="flex" alignItems="center" justifyContent="center">
-                        <Avatar src="/svg.svg" sx={{ width: 48, height: 48, bgcolor: "white", borderRadius: 3 }} />
-                    </Box>
-                    <Box position="absolute" width="72px" height="48px" top="98px" left={0} display="flex" alignItems="center" justifyContent="center">
-                        <Avatar src="/svg-circleicon.svg" sx={{ width: 48, height: 48, bgcolor: "white", borderRadius: 3 }} />
-                    </Box>
-                    <Divider sx={{ position: "absolute", width: "72px", top: "88px", left: 0 }} />
-                    <Box position="absolute" width="72px" height="48px" top="8px" left={0} display="flex" alignItems="center" justifyContent="center">
-                        <Avatar src="/svg-homeicon.svg" sx={{ width: 48, height: 48, bgcolor: "white", borderRadius: 3 }} />
-                    </Box>
-                    <Box position="absolute" width="52px" height="12px" top="10px" left="10px" display="flex" justifyContent="space-between">
-                        <Box width="12px" height="12px" bgcolor="#fc615d" borderRadius="50%" />
-                        <Box width="12px" height="12px" bgcolor="#fdbc40" borderRadius="50%" />
-                        <Box width="12px" height="12px" bgcolor="#34c749" borderRadius="50%" />
-                    </Box>
-                </Box>
-            </Box>
-        </Box>
+                              />
+                              <IconButton>
+                                  <Settings />
+                              </IconButton>
+                              <IconButton>
+                                  <Mic />
+                              </IconButton>
+                              <IconButton>
+                                  <Headset />
+                              </IconButton>
+                              <IconButton>
+                                  <ExitToApp />
+                              </IconButton>
+                          </Box>
+                      </Box>
+                      <Divider />
+                      <Box flexGrow={1} />
+                      <Box display="flex" alignItems="center" px={2} py={1} bgcolor="#ebedef">
+                          <IconButton>
+                              <Avatar src="/div-icon.svg" />
+                          </IconButton>
+                          <Box flexGrow={1} display="flex" alignItems="center" pl={1}>
+                              <Typography variant="body1" color="#747f8d" fontFamily="Helvetica">
+                                  Message #general
+                              </Typography>
+                          </Box>
+                          <IconButton>
+                              <Gif />
+                          </IconButton>
+                          <IconButton>
+                              <Image />
+                          </IconButton>
+                          <IconButton>
+                              <InsertEmoticon />
+                          </IconButton>
+                      </Box>
+                  </Box>
+              </Box>
+          </Box>
+      </Box>
     );
 };
 
